2aa2e81338f80b3482f22281dba645cc1002942d
ALBA (Gigi) REGINA <108318839+Gigi-JAVA-FullStack@users.noreply.github.com>
2023-06-09 14:15:18 -0500

    This refactored code, 5 functions to modularize the different tasks.
    Faco com que o código faça cada análise da lógica de programação em uma função diferente. Retornando as chamadas da função MAIN.

--- desafio-resolvido.py
+++ desafio-resolvido.py
@@ -1,68 +1,90 @@
-# 39784835134
-storage_cpf = [] 
-# cria uma lista vazia para armazenar os números 
- 
-cpf_user = input('Digite números de CPF (sem traço ou ponto): ')
-# entrada CPF do STRING (INTERAVEL)
- 
-cpf_user = cpf_user.replace(".", "").replace("-", "")
-# caso haja, vai sanitizer  pontos e traços
- 
-cpf_str = str(cpf_user)
- 
-cpf_lista = []
-# cria uma nova lista para armazenar os dígitos do CPF como inteiros
- 
-for digito in cpf_str:
-    digito_int = int(digito)
-    # converte o número do CPF em uma lista de numeros inteiros.
-    cpf_lista.append(digito_int)
-    # adiciona o dígito à nova lista de cpf (que estava vazia)
+## SANITIZE CODE: Este código foi refatorado
+## 1) def main(): funcao que chama todas as outras e realiza a lógica MAIN.
+## 2) Função chamada def sanitize_cpf(cpf): sanitiza o CPF string removendo pontos e tracos. 
+## 3) Função chamada def convert_cpf_list(cpf): converter CPF STRING em uma list[] de INTEGERS.
+## 4) Função chamada def validate_cpf(cpf_list): validar cpf usando a primeira e segunda verificacao de DIGITOS -> Return = True if CPF = is valid, else, Return = False.
+## 5) Função chamada get_numeric_input(): solicita a entrada do usuário e valida que apenas números sejam inseridos. 
+## Ele usa o método isdigit para verificar se a entrada consiste apenas em dígitos. 
+## Se a entrada for inválida, o usuário será solicitado novamente até que uma entrada válida seja fornecida.
+## Ao utilizar esta função, garanto que a entrada do CPF seja composta apenas por números, proporcionando melhor experiência e evitando erros no processamento posterior do CPF.
 
-# adiciona cada dígito do CPF na lista Bd 
-storage_cpf.extend(cpf_lista)
-print(storage_cpf) 
- 
-# calcula o primeiro dígito verificador
-contador = 0
-resultado = 0
-controlador = 10
- 
-for numero in storage_cpf[:9]:
-    resultado = numero * controlador
-    contador += resultado
-    controlador -= 1
-    
-contador = ((contador * 10) % 11)
- 
-if contador == 10:
-    contador = 0
- 
-if contador == storage_cpf[9]:
-    contador = contador
-else:
-    print('CPF inválido!')
-    exit()
+def sanitize_cpf(cpf):
+    """
+    Sanitizar o CPF STRING removendo pontos e tracos.
+    """
+    return cpf.replace(".", "").replace("-", "")
 
-storage_cpf.append(contador)
-# adiciona o primeiro dígito verificador à lista 
- 
-# calcula o segundo dígito verificador
-acumulador2 = 0
-resultado2 = 0
-controlador2 = 11
- 
-for numeros in storage_cpf[:10]:
-    resultado2 = numeros * controlador2
-    acumulador2 += resultado2
-    controlador2 -= 1
- 
-acumulador2 = ((acumulador2 * 10) % 11)
- 
-if acumulador2 == 10:
-    acumulador2 = 0
- 
-if acumulador2 == storage_cpf[10]:
-    print('CPF válido!')
-else:
-    print('CPF inválido!')
+def convert_cpf_list(cpf):
+    """
+    Converter CPF STRING em uma list[] de INTEGERS.
+    """
+    cpf_list = []
+    for digito in cpf:
+        digito_int = int(digito)
+        cpf_list.append(digito_int)
+    return cpf_list
+
+def validate_cpf(cpf_list):
+    """
+    Validar cpf usando a primeira e segunda verificacao de DIGITOS -> Return = True if CPF = is valid, else, Return = False.
+    """
+    # Calcular primeira verificacao de DIGIT.
+    i = 0
+    control = 10
+
+    for number in cpf_list[:9]:
+        result = number * control
+        i += result
+        control -= 1
+
+    i = ((i * 10) % 11)
+
+    if i == 10:
+        i = 0
+
+    if i != cpf_list[9]:
+        return False
+
+    # Calcular segunda verificacao de DIGIT.
+    j = 0
+    control = 11
+
+    for number in cpf_list[:10]:
+        result = number * control
+        j += result
+        control -= 1
+
+    j = ((j * 10) % 11)
+
+    if j == 10:
+        j = 0
+
+    if j != cpf_list[10]:
+        return False
+    return True
+
+def get_numeric_input(prompt):
+    """
+    PROMPT USER para INPUT e VALIDACAO, apenas permite entrada de numeros. 
+    """
+    while True:
+        user_input = input(prompt)
+        if user_input.isdigit():
+            return user_input
+        else:
+            print("\nInvalido. Por favor, digite apenas numeros.")
+
+def main():
+    storage_cpf = []
+    cpf_user_input = get_numeric_input('\nDigite números de CPF (sem traço ou ponto): ')
+    cpf_user = sanitize_cpf(cpf_user_input)
+    cpf_list = convert_cpf_list(cpf_user)
+    storage_cpf.extend(cpf_list)
+
+    if validate_cpf(storage_cpf):
+        print('\nCPF válido!')
+    else:
+        print('\nCPF inválido!')
+
+if __name__ == "__main__":
+    main()
